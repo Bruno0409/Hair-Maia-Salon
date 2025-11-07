c876ace042b3f645c695fe9cd08532981a55f841
bruno0409 <brunooliveiradesign@hotmail.com>
2025-11-07 14:28:41 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,84 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 2000,
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+  });
+})(jQuery);
+
+const carousel = document.querySelector(".image-carousel");
+
+let scrollAmount = 0;
+function autoScroll() {
+  scrollAmount += carousel.offsetWidth * 0.8 + 20; // largura do slide + gap
+  if (scrollAmount >= carousel.scrollWidth) scrollAmount = 0;
+  carousel.scrollTo({ left: scrollAmount, behavior: "smooth" });
+}
+
+setInterval(autoScroll, 3000); // muda a cada 3s
+
+document.querySelectorAll(".faq-item").forEach((item) => {
+  item.addEventListener("click", () => {
+    // Fechar outros itens
+    document.querySelectorAll(".faq-item").forEach((i) => {
+      if (i !== item) {
+        i.classList.remove("active");
+        i.querySelector(".faq-answer").style.maxHeight = null;
+      }
+    });
+
+    // Abrir ou fechar item clicado
+    item.classList.toggle("active");
+    const answer = item.querySelector(".faq-answer");
+    if (item.classList.contains("active")) {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = null;
+    }
+  });
+});
